24ae589f0d927ae078217995e4ad4325285eef42
Aman Goswami <aman@nworx.ai>
2026-06-13 16:48:27 +0530

    Added: Spring boot starter project.

---
+++ src/main/java/com/example/PaymeshLedger/PaymeshLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaymeshLedger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymeshLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymeshLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PaymeshLedger/PaymeshLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaymeshLedger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymeshLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
